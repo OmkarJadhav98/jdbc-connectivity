d3f4542ffbdf3332c0cabbb0f0eae5d76bad13f5
Omkar Jadhav <objadhav28@gmail.com>
2024-07-24 18:05:15 +0530

    No commit Message

--- src/main/java/com/project/model/Address.java
+++ src/main/java/com/project/model/Address.java
@@ -1,16 +1,38 @@
 package com.project.model;
 
 public class Address {
-
-    private int addressId;
+    private long id;
+    private String street;
     private String city;
+    private String state;
+    private String country;
+    private long postalCode;
 
-    public int getAddressId() {
-        return addressId;
+    public Address(long id, String street, String city, String state, String country, long postalCode) {
+        this.id = id;
+        this.street = street;
+        this.city = city;
+        this.state = state;
+        this.country = country;
+        this.postalCode = postalCode;
     }
 
-    public void setAddressId(int addressId) {
-        this.addressId = addressId;
+    // Getters and Setters
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getStreet() {
+        return street;
+    }
+
+    public void setStreet(String street) {
+        this.street = street;
     }
 
     public String getCity() {
@@ -21,11 +43,27 @@ public class Address {
         this.city = city;
     }
 
-    public Address() {
+    public String getState() {
+        return state;
     }
 
-    public Address(int addressId, String city) {
-        this.addressId = addressId;
-        this.city = city;
+    public void setState(String state) {
+        this.state = state;
+    }
+
+    public String getCountry() {
+        return country;
+    }
+
+    public void setCountry(String country) {
+        this.country = country;
+    }
+
+    public long getPostalCode() {
+        return postalCode;
+    }
+
+    public void setPostalCode(long postalCode) {
+        this.postalCode = postalCode;
     }
 }
